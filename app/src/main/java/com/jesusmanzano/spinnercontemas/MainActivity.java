33825d6603322c9ecd630bea8ffa2b9027cfd719
JesusManzano24 <9991404825j@gmail.com>
2024-06-19 06:26:23 -0600

    Segundo Commit

--- app/src/main/java/com/jesusmanzano/spinnercontemas/MainActivity.java
+++ app/src/main/java/com/jesusmanzano/spinnercontemas/MainActivity.java
@@ -1,6 +1,8 @@
 package com.jesusmanzano.spinnercontemas;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,22 +20,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    Spinner spinner;
-private EditText user, Contraseña;
-    TextView textViewDescripcion;
+    private static final String PREF_THEME = "pref_theme";
+    private Spinner spinner;
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // Verificar si es la primera vez que se inicia la app
+        if (!prefs.contains(PREF_THEME)) {
+            prefs.edit().putInt(PREF_THEME, R.style.AppTheme).apply();
+        }
+
+        int themeId = prefs.getInt(PREF_THEME, R.style.AppTheme);
+        setTheme(themeId);
+
         setContentView(R.layout.activity_main);
+
         spinner = (Spinner) findViewById(R.id.Spinnertemas);
-        textViewDescripcion = (TextView) findViewById(R.id.themeDescription);
 
         final String[] Temas = {
-                "Selecciona un Tema", "Tema1", "Tema2", "Tema3"
+                "Selecciona un tema", "Tema1", "Tema2", "Tema3"
         };
 
-        // Configurar el adaptador para el Spinner
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Temas);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
@@ -41,29 +53,31 @@ private EditText user, Contraseña;
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if(position > 0) {
-                    int imageId = getResources().getIdentifier(Temas[position].toLowerCase(), "drawable", getPackageName());
-
-                    Toast.makeText(parent.getContext(), "Seleccionaste el tema: " + Temas[position], Toast.LENGTH_SHORT).show();
-
-
-                    switch (position) {
-                        case 1:
-                            setTheme(R.style.Tema1);
-                            break;
-                        case 2: // Venus
-                            setTheme(R.style.Tema2);
-                            break;
+                int newThemeId;
+                switch (position) {
+                    case 0:
+                        newThemeId = R.style.AppTheme;
+                        break;
+                    case 1:
+                        newThemeId = R.style.Tema1;
+                        break;
+                    case 2:
+                        newThemeId = R.style.Tema2;
+                        break;
                         case 3:
-                            setTheme(R.style.Tema3);
-                    }
+                            newThemeId = R.style.Tema3;
+                        break;
+                    default:
+                        return;
+                }
 
-                    // Reiniciar la actividad para aplicar el tema
-                    finish();
-                    startActivity(getIntent());
-                } else {
+                int currentThemeId = prefs.getInt(PREF_THEME, R.style.AppTheme);
+                if (currentThemeId != newThemeId) {
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putInt(PREF_THEME, newThemeId);
+                    editor.apply();
 
-                    textViewDescripcion.setText("");
+                    recreate();
                 }
             }
 
@@ -74,4 +88,3 @@ private EditText user, Contraseña;
         });
     }
 }
-
